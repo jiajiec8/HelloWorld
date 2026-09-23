7e65f6db470ea349a870a9687b945c58a679134d
Jia Jie <jiajiec8@nycstudents.net>
2026-09-23 15:14:09 -0400

    Added Scanner

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class HelloWorld {
     public static void main(String[]args){
         System.out.println("What's your name?");
         Scanner s = new Scanner(System.in);
     String name = s.nextLine();
-System.out.println("Hello," + name + "!");}
-}
+System.out.println("Hello," + name + "!");
+File f=new File("AP Computer Science A");
+s.close();
+s=new Scanner(f);
+String course=s.nextLine();
+System.out.println("Welcome to " + course + "!");
+    }}
